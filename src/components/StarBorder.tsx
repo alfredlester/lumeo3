b383f0f11d6037cafaa6357c4f05b61abea0a28a
alfredlester <alfredlester657@gmail.com>
2025-11-10 17:51:34 +0000

    Updated StarBorder.tsx

--- src/components/StarBorder.tsx
+++ src/components/StarBorder.tsx
@@ -14,7 +14,7 @@ const StarBorder = <T extends React.ElementType = 'button'>({
   className = '',
   color = 'white',
   speed = '5s',
-  thickness = 1,
+  thickness = 4,
   children,
   ...rest
 }: StarBorderProps<T>) => {
